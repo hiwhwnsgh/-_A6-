f5c532669cb3ec2d696c59cf2e7ae58a2b5d3b94
RyuJunho <hoo9902@naver.com>
2022-06-04 12:32:20 +0900

    클래스화(수정예정)

--- JobListUI.py
+++ JobListUI.py
@@ -1,55 +1,59 @@
 from tkinter import *
 from tkinter.ttk import*
 
-root = Tk()
-root.title("직업 목록 조회")
-root.geometry("500x300")
-root.resizable(False,False)
+class JobListUI (Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.title("직업 목록 조회")
+        self.geometry("500x300")
+        self.resizable(False,False)
 
-label = Label(root,text='직억 목록 조회')
-label.pack(pady=10)
-#테이블과 스크롤바를 넣을 프레임 생성
-frame = Frame(root)
+        self.label = Label(self,text='직억 목록 조회')
+        self.label.pack(pady=10)
+        #테이블과 스크롤바를 넣을 프레임 생성
+        frame = Frame(self)
 
-#테이블 생성
-treeview = Treeview(frame, selectmode = 'browse')
-treeview['columns'] = ('one','two','three')
-treeview['show'] = 'headings'
-treeview.pack(side='left')
+        #테이블 생성
+        self.JobListTable = Treeview(frame, selectmode = 'browse')
+        self.JobListTable['columns'] = ('one','two','three')
+        self.JobListTable['show'] = 'headings'
+        self.JobListTable.pack(side='left')
 
-#스크롤바 생성
-scrollbar = Scrollbar(frame,command=treeview.yview)
-scrollbar.pack(side='right',fill='y')
-treeview.configure(yscrollcommand=scrollbar.set)
+        #스크롤바 생성
+        self.scrollbar = Scrollbar(frame,command=self.JobListTable.yview)
+        self.scrollbar.pack(side='right',fill='y')
+        self.JobListTable.configure(yscrollcommand=self.scrollbar.set)
 
-#테이블 속성 설정
-treeview.column("one",width=100,)
-treeview.heading("one",text = "직업이름",anchor="center")
+        #테이블 속성 설정
+        self.JobListTable.column("one",width=100,)
+        self.JobListTable.heading("one",text = "직업이름",anchor="center")
 
-treeview.column("two",width=100,)
-treeview.heading("two",text = "관련학과",anchor="center")
+        self.JobListTable.column("two",width=100,)
+        self.JobListTable.heading("two",text = "관련학과",anchor="center")
 
-treeview.column("three",width=100,)
-treeview.heading("three",text = "전망",anchor="center")
+        self.JobListTable.column("three",width=100,)
+        self.JobListTable.heading("three",text = "전망",anchor="center")
 
-#테이블 값 입력
-job_list = [('웹개발자','컴퓨터공학과','증가'),
-            ('한의사','한의학과','현상유지'),
-            ('소설가','국어국문학과','감소'),
-            ('미용사','뷰티아트과','현상유지'),
-            ('스포츠강사','체육학과','현상유지'),
-            ('판사','법학과','현상유지'),
-            ('항해사','해양공학과','감소'),
-            ('웹개발자','컴퓨터공학과','증가'),
-            ('한의사','한의학과','현상유지'),
-            ('소설가','국어국문학과','감소'),
-            ('미용사','뷰티아트과','현상유지'),
-            ('스포츠강사','체육학과','현상유지'),
-            ('판사','법학과','현상유지'),
-            ('항해사','해양공학과','감소')]
+        #테이블 값 입력
+        job_list = [('웹개발자','컴퓨터공학과','증가'),
+                    ('한의사','한의학과','현상유지'),
+                    ('소설가','국어국문학과','감소'),
+                    ('미용사','뷰티아트과','현상유지'),
+                    ('스포츠강사','체육학과','현상유지'),
+                    ('판사','법학과','현상유지'),
+                    ('항해사','해양공학과','감소'),
+                    ('웹개발자','컴퓨터공학과','증가'),
+                    ('한의사','한의학과','현상유지'),
+                    ('소설가','국어국문학과','감소'),
+                    ('미용사','뷰티아트과','현상유지'),
+                    ('스포츠강사','체육학과','현상유지'),
+                    ('판사','법학과','현상유지'),
+                    ('항해사','해양공학과','감소')]
 
-for i in range(len(job_list)):
-    treeview.insert('','end',values=job_list[i])
-    
-frame.pack(pady=10)
-root.mainloop()
+        for i in range(len(job_list)):
+            self.JobListTable.insert('','end',values=job_list[i])
+
+        frame.pack(pady=10)
+
+app = JobListUI()
+app.mainloop()
