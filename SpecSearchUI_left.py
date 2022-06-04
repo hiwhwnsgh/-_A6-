412240b33db3bc7fedc383f872aa824f39386118
RyuJunho <hoo9902@naver.com>
2022-06-04 19:57:40 +0900

    스펙검색 구현(거의완료)

--- SpecSearchUI_left.py
+++ SpecSearchUI_left.py
@@ -1,12 +1,17 @@
 from tkinter import *
 from tkinter.ttk import *
 from SpecSearchUI_right import*
+import pandas as pd
+
+DIR_CSV_JOB = "Job.csv"
 
 class SpecSearchUI (Tk) :
     def __init__(self):
         Tk.__init__(self)
+        #self.window = Tk()
+        self.action= SpecSearchSystem()
         self.title("스펙 검색 조회")
-        self.geometry("700x400")
+        self.geometry("800x400")
         self.resizable(False, False)
         # 왼쪽 검색창
         left_frame = Frame(self,width = 200)
@@ -14,9 +19,10 @@ class SpecSearchUI (Tk) :
         # 검색창(Entry, Button) 프레임
         Search_frame = Frame(left_frame)
 
-        self.SearchEntry = Entry(Search_frame, width=25)  # 검색창
+        self.SearchEntry = Entry(Search_frame, width=20)  # 검색창
         self.SearchEntry.grid(row=1, column=1)
-        self.SearchButton = Button(Search_frame, width=5, text="찾기")  # 검색버튼
+        self.SearchButton = Button(Search_frame, width=5, text="찾기"
+                                   ,command= lambda : self.action.SearchClick(self.SearchEntry.get(),self.SearchTable))  # 검색버튼
         self.SearchButton.grid(row=1, column=2)
 
         Search_frame.pack(padx=50, pady=10)
@@ -40,36 +46,42 @@ class SpecSearchUI (Tk) :
         self.SearchTable.column("one", width=200, )
         self.SearchTable.heading("one", text="직업", anchor="center")
 
-        # 테이블 값 입력
-        job_list = [('웹개발자'),
-                ('한의사'),
-                ('소설가'),
-                ('미용사'),
-                ('스포츠강사'),
-                ('판사'),
-                ('항해사'),
-                ('웹개발자'),
-                ('한의사'),
-                ('소설가'),
-                ('미용사'),
-                ('스포츠강사'),
-                ('판사'),
-                ('항해사')]
-
-        for i in range(len(job_list)):
-            self.SearchTable.insert('', 'end', values=job_list[i])
-
         result_frame.pack(padx=50)  # 검색 결과 테이블 프레임 부착
 
         #선택버튼
-        self.ChoiceButton = Button(left_frame, text = '선택',width = 30,command=lambda:self.search('웹개발자'))
+        self.ChoiceButton = Button(left_frame, text = '선택',width = 30
+                                   ,command=lambda:self.choice(self.SearchTable.focus()))
         self.ChoiceButton.pack(pady=40)
 
+        #오른쪽 프레임
         self.right_frame = Frame(self)
-        self.right_frame.pack(side='right')
+        self.right_frame.pack(side='right',expand=True)
+        self.frame = SpecSearchUI_right(parent=self.right_frame, controller=self)   #오른쪽 화면 객체
+        self.right_frame.pack_forget()
 
-    def search(self,job_name):
-        frame = SpeckSearchUI_right(self.right_frame,controller=self)
+
+    def choice(self,job_name):  #선택버튼시 호출되는 이벤트
+        self.frame.modify(job_name)
+        self.right_frame.pack(side='right',expand=True)
+
+#컨트롤 클래스
+class SpecSearchSystem() :
+    def __init__(self):
+        jobDB = JobDB()
+        self.df_Job = jobDB.get_df()
+
+    def SearchClick(self,word,treeView):
+        # 테이블 초기화
+        for item in treeView.get_children():
+            treeView.delete(item)
+
+        # 검색어(자격증)가 포함된 df
+        df_contains = self.df_Job[self.df_Job['jobCertifi'].str.contains(word)]
+
+        # 추출한 df에서 직업이름을 Table에 추가
+        for select_name in df_contains['jobName']:
+            name= df_contains['jobName'].loc[select_name]
+            treeView.insert('','end',text='',value=name,iid=name)
 
 
 app = SpecSearchUI()
--- SpecSearchUI_right.py
+++ SpecSearchUI_right.py
@@ -1,11 +1,16 @@
 from tkinter import *
 from tkinter.ttk import *
+import pandas as pd
 
-class SpeckSearchUI_right(Frame) :
+class SpecSearchUI_right(Frame) :
     def __init__(self,parent,controller):
+        print('우측화면생성')
         Frame.__init__(self,parent)
         self.controller = controller
         self.parent= parent
+
+        jobDB = JobDB()
+        self.df_Job = jobDB.get_df()
 
         # 검색창(Entry, Button) 프레임
         job_name_frame = Frame(self.parent)
@@ -24,19 +29,19 @@ class SpeckSearchUI_right(Frame) :
         self.SearchTable['columns'] = ('one','two','three')
         self.SearchTable['show'] = 'headings'
 
-        self.SearchTable.pack()
+        self.SearchTable.pack(padx=20)
 
         # 테이블 속성 설정
         self.SearchTable.column("one", width=50,anchor='center' )
         self.SearchTable.heading("one", text="자격증", anchor="center")
 
-        self.SearchTable.column("two", width=150, )
+        self.SearchTable.column("two", width=180, )
         self.SearchTable.heading("two", text="회원 SPEC", anchor="center")
 
-        self.SearchTable.column("three", width=150, )
+        self.SearchTable.column("three", width=180, )
         self.SearchTable.heading("three", text="직업", anchor="center")
         # 테이블 값 입력
-        job_list = [('필수','없음','신변보호사\n(국가공인 민간)'),
+        job_list = [('필수','없음',''),
                     ('관련','자동차운전면허 2종 보통\n(국가전문)','일반경비지도사\n(국가전문)\n자동차운전면허 제 1종,2종 보통\n(국가전문)'),
                     ('TOEIC','500점','700점(가산점)'),
                     ]
@@ -45,6 +50,29 @@ class SpeckSearchUI_right(Frame) :
             self.SearchTable.insert('', 'end', values=job_list[i])
 
         #확인버튼
-        self.Confirm_Button = Button(self.parent, text = '확인',width = 30)
+        self.Confirm_Button = Button(self.parent, text = '확인',width = 30,command= lambda : self.forget())
         self.Confirm_Button.pack(pady=40)
 
+    def forget(self):
+        self.parent.pack_forget()
+
+    def modify(self,job_name):
+        for item in self.SearchTable.get_children():
+            self.SearchTable.delete(item)
+
+        # 테이블 값 입력
+        job_list = [('필수','없음',self.df_Job['jobCertifi'].loc[job_name]),
+                    ('관련','자동차운전면허 2종 보통\n(국가전문)',self.df_Job['jobCertifi'].loc[job_name]),
+                    ('TOEIC','500점',self.df_Job['jobToeic'].loc[job_name]),
+                    ]
+
+        for i in range(len(job_list)):
+            self.SearchTable.insert('', 'end', values=job_list[i])
+
+
+class JobDB():
+    def __init__(self):
+        df_Job = pd.read_csv("csv/Job.csv",encoding='CP949')
+        self.df_Job = df_Job.set_index('jobName',drop=False)
+    def get_df(self):
+        return self.df_Job
