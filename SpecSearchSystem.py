4da23e0ffd68a2a7e10bfc18ef9cbfc96e148180
RyuJunho <hoo9902@naver.com>
2022-06-06 15:30:52 +0900

    검색 예외처리

--- SpecSearchSystem.py
+++ SpecSearchSystem.py
@@ -1,4 +1,6 @@
 from SpecSearchUI_right import*
+import tkinter.messagebox as msg
+
 
 #컨트롤 클래스
 class SpecSearchSystem() :
@@ -14,8 +16,10 @@ class SpecSearchSystem() :
 
         # 검색어(자격증)가 포함된 df
         df_contains = self.df_Job[self.df_Job['jobCertifi'].str.contains(word)]
-
-        # 추출한 df에서 직업이름을 Table에 추가
-        for select_name in df_contains['jobName']:
-            name= df_contains['jobName'].loc[select_name]
-            treeView.insert('','end',text='',value=name,iid=name)
+        if df_contains.empty:
+            msg.showinfo('입력오류','존재하지 않는 자격증입니다.')
+        else :
+            # 추출한 df에서 직업이름을 Table에 추가
+            for select_name in df_contains['jobName']:
+                name= df_contains['jobName'].loc[select_name]
+                treeView.insert('','end',text='',value=name,iid=name)
